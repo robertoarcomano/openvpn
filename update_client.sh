20cfa15af6e942109ac433843b4cf08f30c21bcc
Roberto Arcomano <bertolinux@gmail.com>
2020-09-04 23:11:08 +0100

    debug 27

--- update_client.sh
+++ update_client.sh
@@ -6,12 +6,13 @@ if [ -z "$1" ]; then
 fi
 
 DIR=`dirname $0`
+DIR_CLIENT=$DIR/openvpn/$CLIENT_NAME
 CLIENT_NAME=$1
 CONF=$DIR/client.conf
 CA=$(cat $DIR/ca.crt)
-CERT=$(cat $DIR/$CLIENT_NAME.crt)
-KEY=$(cat $DIR/$CLIENT_NAME.key)
-TLS_AUTH=$(cat $DIR/ta.key)
+CERT=$(cat $DIR_CLIENT/$CLIENT_NAME.crt)
+KEY=$(cat $DIR_CLIENT/$CLIENT_NAME.key)
+TLS_AUTH=$(cat $DIR_CLIENT/ta.key)
 sudo sed -ri "s/<ca><\/ca>/<ca>$CA<\/ca>/" $CONF
 sudo sed -ri "s/<cert><\/cert>/<cert>$CERT<\/cert>/" $CONF
 sudo sed -ri "s/<key><\/key>/<key>$KEY<\/key>/" $CONF
