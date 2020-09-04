932e97a8214efbd40de239fb1f0e92958066874c
Roberto Arcomano <bertolinux@gmail.com>
2020-09-04 22:49:15 +0100

    debug 22

--- update_client.sh
+++ update_client.sh
@@ -5,13 +5,15 @@ if [ -z "$1" ]; then
   exit
 fi
 
+IR=`dirname $0`
 CLIENT_NAME=$1
+CONF=$DIR/client.conf
 
 CA=$(cat ca.crt)
 CERT=$(cat $CLIENT_NAME.crt)
 KEY=$(cat $CLIENT_NAME.key)
 TLS_AUTH=$(cat ta.key)
-sudo sed -ri "s/<ca><\/ca>/<ca>$CA<\/ca>/" client.conf
-sudo sed -ri "s/<cert><\/cert>/<cert>$CERT<\/cert>/" client.conf
-sudo sed -ri "s/<key><\/key>/<key>$KEY<\/key>/" client.conf
-sudo sed -ri "s/<tls-auth><\/tls-auth>/<tls-auth>$TLS_AUTH<\/tls-auth>/" client.conf
+sudo sed -ri "s/<ca><\/ca>/<ca>$CA<\/ca>/" $CONF
+sudo sed -ri "s/<cert><\/cert>/<cert>$CERT<\/cert>/" $CONF
+sudo sed -ri "s/<key><\/key>/<key>$KEY<\/key>/" $CONF
+sudo sed -ri "s/<tls-auth><\/tls-auth>/<tls-auth>$TLS_AUTH<\/tls-auth>/" $CONF
